a685879f8144c3d032c5343d530556a0fa4ece33
坏黑 <b@skymc.me>
2021-08-31 16:17:00 +0800

    update

--- src/main/java/ink/ptms/atlantis/AtlantisMod.java
+++ src/main/java/ink/ptms/atlantis/AtlantisMod.java
@@ -1,7 +1,13 @@
 package ink.ptms.atlantis;
 
+import com.mojang.realmsclient.gui.screens.RealmsResetWorldScreen;
+import net.minecraft.client.gui.screen.LanguageScreen;
+import net.minecraft.resources.ResourcePackList;
+import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("atlantis_mod")
 public class AtlantisMod {
+    public AtlantisMod() {
+    }
 }
